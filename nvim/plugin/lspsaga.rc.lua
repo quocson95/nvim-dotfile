c8765aed4c1f7c4adac9f9f79aaf7ce088c0aec7
DANGQUOCSON <dangquocson1995@gmail.com>
2023-03-14 20:42:25 +0700

    init

---
+++ nvim/plugin/lspsaga.rc.lua
@@ -0,0 +1,42 @@
+local status, saga = pcall(require, "lspsaga")
+if (not status) then return end
+
+saga.setup({
+  ui = {
+    winblend = 10,
+    border = 'rounded',
+    colors = {
+      normal_bg = '#002b36'
+    }
+  }
+})
+
+local diagnostic = require("lspsaga.diagnostic")
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', 'gl', '<Cmd>Lspsaga show_diagnostic<CR>', opts)
+vim.keymap.set("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+-- Show cursor diagnostics
+-- Like show_line_diagnostics, it supports passing the ++unfocus argument
+vim.keymap.set("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+
+-- Show buffer diagnostics
+vim.keymap.set("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+-- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
+vim.keymap.set('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
+vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+
+-- Call hierarchy
+vim.keymap.set("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+vim.keymap.set("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+
+-- code action
+local codeaction = require("lspsaga.codeaction")
+vim.keymap.set("n", "<leader>ca", function() codeaction:code_action() end, { silent = true })
+vim.keymap.set("v", "<leader>ca", function()
+  vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-U>", true, false, true))
+  codeaction:range_code_action()
+end, { silent = true })
