8e4982e75438a7e417603498ad5523e8651f54a3
DANGQUOCSON <dangquocson1995@gmail.com>
2023-03-17 21:36:39 +0700

    [update]: setting nvim-tree

--- nvim/after/plugin/treeview.rc.lua
+++ nvim/after/plugin/treeview.rc.lua
@@ -1,5 +1,18 @@
 local ntree = require('nvim-tree.api')
 
+require("nvim-tree").setup {
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = true,
+    ignore_list = {},
+  },
+}
+
 vim.keymap.set('n', 'nn', function()
   ntree.tree.toggle()
 end)
